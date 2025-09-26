b349dfcb238e7935d2d3e2cb240e2396f58aa6cd
tim-1965 <mymoscowcams@gmail.com>
2025-09-26 20:40:16 +0100

    Add files via upload

--- public/AppController.js
+++ public/AppController.js
@@ -1,3 +1,6 @@
 // Thin re-export for backwards compatibility with legacy bundle paths.
+// This file lives at /public/AppController.js and intentionally re-exports
+// the module from the /public/components directory so that legacy imports
+// (which expect the component bundle in /components) continue to function.
 export * from './components/AppController.module.js';
 export { AppController as default } from './components/AppController.module.js';
